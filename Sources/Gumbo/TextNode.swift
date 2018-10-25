9b3e943e9b85d57bc011bd778ee616696023dfb7
Trevör Anne Denise <trevor.annedenise@icloud.com>
2018-10-25 23:43:16 +0200

    Fix wrong memory access in TextNode

--- Sources/Gumbo/TextNode.swift
+++ Sources/Gumbo/TextNode.swift
@@ -4,15 +4,11 @@ import CGumbo
 public class TextNode: Node {
     
     var node: GumboNode
-    public var text: String {
-        return String(cString: node.v.text.text)
-    }
-    public var originalText: String {
-        return String(cString: node.v.text.original_text.data)
-    }
+    public var text: String
     
     override init(gumboNode: GumboNode, parent: Node? = nil, indexWithinParent: Int? = nil) {
         self.node = gumboNode
+        self.text = String(cString: node.v.text.text)
         super.init(gumboNode: gumboNode, parent: parent, indexWithinParent: indexWithinParent)
     }
 }
